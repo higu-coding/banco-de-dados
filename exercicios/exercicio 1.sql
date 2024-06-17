9ef348089cabf78749a95daca8c8421b2057f6b5
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-06-16 23:20:13 -0300

    nova tabela

---
+++ exercicios/exercicio 1.sql
@@ -0,0 +1,25 @@
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR (100),
+    email VARCHAR (100),
+    telefone VARCHAR (20),
+    endereco VARCHAR(100)
+);
+
+CREATE TABLE carros (
+    id SERIAL PRIMARY KEY,
+    marca VARCHAR (50),
+    modelo VARCHAR (50),
+    ano INT,
+    cor VARCHAR (20),
+    preco DECIMAL (10, 2)
+);
+
+CREATE TABLE vendas (
+    id SERIAL PRIMARY KEY,
+    carro_id INT REFERENCES carros(id),
+    cliente_id INT REFERENCES clientes(id),
+    data_venda DATE,
+    valor DECIMAL(10, 2)
+);
+
