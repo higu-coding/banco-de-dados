c4c9b612c8bff0ed2bab0d16702517444cd7d702
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-07-01 17:11:02 -0300

    modelo de notas usando Associações

--- conectando no banco de dados/index.js
+++ conectando no banco de dados/index.js
@@ -1,5 +1,6 @@
 // Estabelecendo conexão com o banco de dados
 
+const { DataTypes } = require('sequelize');
 const sequelize = require('./config/database');
 const Usuario = require('./models/usuario');
 
@@ -12,12 +13,12 @@ try {
     // CRUD de usuarios - Dados ficticios*
 
     // C = CREATE
-    const createUsuario = await Usuario.create({
+    const novoUsuario = await Usuario.create({
         nome: 'Fernando',
         email: 'fernando@example.com',
         senha: 'fernando123',
     });
-    console.log('Usuário inserido com sucesso:', createUsuario.toJSON());
+    console.log('Usuário inserido com sucesso:', novoUsuario.toJSON());
 
     // R = READ
     const readUsuario = await Usuario.findAll();
@@ -33,6 +34,32 @@ try {
     await updateUsuario.destroy();
     console.log('Usuário excluído com sucesso.');
 
+    // Modelo de Notas - Usando ASSOCIAÇÕES
+    const Nota = sequelize.define('Nota', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        valor: DataTypes.INTEGER,
+        disciplina: DataTypes.STRING
+    });
+
+    Usuario.hasMany(Nota);
+    Nota.belongsTo(Usuario);
+
+    // Consulta para obter um usuário com suas notas
+    Usuario.findByPk(1, {
+        include: Nota, //Especifica a inclusão do modelo de nota
+    }).then((usuario) => {
+        console.log('Usuário:', usuario.toJSON());
+        console.log('Notas do usuário:');
+        usuario.Notas.forEach((Nota) => {
+            console.log('Disciplina:', Nota.disciplina, 'Valor:', Nota.valor);
+        });
+        console.log('Uusário não encontrado.');
+    });
+    
 } catch (error) {
     console.error('Erro ao conectar/ inserir no banco de dados: ', error);
 } finally {
